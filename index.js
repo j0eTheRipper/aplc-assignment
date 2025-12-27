d6dae57c69472f9bbf3e2211ac89afe3f028ab6d
j0eTheRipper <j0etheripper0010@gmail.com>
2025-12-28 05:34:28 +0800

    refactored and made a package

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 class Vehicle {
-  constructor(id, manufacturer, model, year, batteryType, batteryCapacity, range, chargingType, chargeTime, price, color, countryOfManufacture, autonomousLevel, co2Emmission, safetyRating, unitsSold, warranty) {
+  constructor(
+    id,
+    manufacturer,
+    model,
+    year,
+    batteryType,
+    batteryCapacity,
+    range,
+    chargingType,
+    chargeTime,
+    price,
+    color,
+    countryOfManufacture,
+    autonomousLevel,
+    co2Emmission,
+    safetyRating,
+    unitsSold,
+    warranty,
+  ) {
     this.id = id;
     this.manufacturer = manufacturer;
     this.model = model;
@@ -37,16 +55,15 @@ class Vehicle {
       obj.CO2_Emissions_g_per_km,
       obj.Safety_Rating,
       obj.Units_Sold_2024,
-      obj.Warranty_Years
+      obj.Warranty_Years,
     );
   }
 }
 
-
 let vehicles = [];
 
 async function getData() {
-  const request = await fetch('data.json');
+  const request = await fetch("data.json");
   const response = await request.json();
 
   for (let i = 0; i < response.length; i++) {
@@ -56,41 +73,62 @@ async function getData() {
 
 getData();
 
-async function getTotalManufacturedByEachCompany() {
-  document.getElementById('totalManufacturedOut')?.remove();  // remove the result <p> if it exists. The ? is a null check
+function getTotalManufacturedByEachCompany() {
+  const resultDivId = "totalManufacturedOut";
+  document.getElementById(resultDivId)?.remove(); // remove the result <p> if it exists. The ? is a null check
 
-  const input = document.getElementsByName('totalManufacturedInput')[0].value.toLowerCase();
+  const input = document
+    .getElementsByName("totalManufacturedInput")[0]
+    .value.toLowerCase();
 
   let result = 0;
   for (car of vehicles)
-    (car.manufacturer.toLowerCase() === input || car.model.toLowerCase() === input) && result++;
+    (car.manufacturer.toLowerCase() === input ||
+      car.model.toLowerCase() === input) &&
+      result++;
 
-  const div = document.getElementById('totalByEachCompany');
-  const p = document.createElement('p');
-
-  p.append(`number of cars by ${input}: ${result}`);
-  p.setAttribute('id', 'totalManufacturedOut')
-  div.append(p)
+  const resultDiv = ResultLine("totalByEachCompany", resultDivId);
+  resultDiv.append(`number of cars by ${input}: ${result}`);
 }
 
-async function getModelsByCompany() {
-  document.getElementById('modlesByCompanyOut')?.remove();
-  const input = document.getElementsByName('listOfModelsInput')[0].value.toLowerCase();
+function getModelsByCompany() {
+  const resultDiv = "modlesByCompanyOut";
+  document.getElementById(resultDiv)?.remove();
+  const input = document
+    .getElementsByName("listOfModelsInput")[0]
+    .value.toLowerCase();
 
-  const div = document.getElementById('listOfModels');
-  const resultDiv = document.createElement('div');
-  resultDiv.setAttribute('class', 'resultBox');
-  resultDiv.setAttribute('id', 'modlesByCompanyOut');
-  div.appendChild(resultDiv);
-  const list = document.createElement('ul');
-  resultDiv.appendChild(list);
+  const list = ResultList("listOfModels", resultDiv);
 
   for (car of vehicles) {
-    if (car.manufacturer.toLowerCase() === input && !resultDiv.innerText.includes(car.model)) {
-      console.log(car.manufacturer);
-      const element = document.createElement('li');
+    const isRightManufacturer = car.manufacturer.toLowerCase() === input;
+    const isNotAdded = !list.innerHTML.includes(car.model);
+
+    if (isRightManufacturer && isNotAdded) {
+      const element = document.createElement("li");
       element.append(car.model);
       list.appendChild(element);
     }
   }
+}
+
+function ResultList(parentId, id) {
+  const div = document.getElementById(parentId);
+  const resultDiv = document.createElement("div");
+  resultDiv.setAttribute("class", "resultBox");
+  resultDiv.setAttribute("id", id);
+  div.appendChild(resultDiv);
+  const list = document.createElement("ul");
+  resultDiv.appendChild(list);
+
+  return list;
+}
+
+function ResultLine(parentId, id) {
+  const div = document.getElementById(parentId);
+  const resultDiv = document.createElement("div");
+  resultDiv.setAttribute("id", id);
+  div.append(resultDiv);
+
+  return resultDiv;
 }
